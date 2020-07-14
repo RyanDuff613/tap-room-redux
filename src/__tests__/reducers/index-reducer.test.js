384553f32a08e306c82e975b8c7837aceb94dda8
Ryan Duff <ryanduff613@gmail.com>
2020-07-13 23:30:55 -0700

    create tests for passing actions into rootReducer, all tests passing

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -22,4 +22,25 @@ describe('rootReducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
 
+  test('initial state of kegListReducer matches rootReducer when action type provided', () => {
+    const action = {
+      type: 'ADD_KEG',
+      brand: 'ninkasi',
+      name: 'believer',
+      price: '$5.00',
+      alcoholContent: '8%',
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, action));
+  });
+
+  test('initial state of formVisibleReducer matches rootReducer when action type provided', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
+
 });
