61b001878f298eb37afb757178b1c37996c9b60a
Ryan Duff <ryanduff613@gmail.com>
2020-07-13 21:20:30 -0700

    create switch/case for adding keg in kegListReducer, create test for reducer action, all tests passing

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,40 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+
+  let action;
+  const kegData = {
+    brand: 'ninkasi',
+    name: 'believer',
+    price: '$5.00',
+    alcoholContent: '8%',
+    id: 1
+  };
+
   test('returns default state if no action type is supplied', () => {
     expect(kegListReducer({}, {type: null})).toEqual({});
   });
+
+  test('should add new keg information to masterKegList', () => {
+    const {brand, name, price, alcoholContent, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      brand: brand,
+      name: name,
+      price: price,
+      alcoholContent: alcoholContent,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        brand: brand,
+        name: name,
+        price: price,
+        alcoholContent: alcoholContent,
+        id: id
+      }
+    });
+  });
+
 });
