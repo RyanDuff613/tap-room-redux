77e6c47629b8d4ff5c1a3369638f2c81c2abbb9e
Ryan Duff <ryanduff613@gmail.com>
2020-07-13 23:00:41 -0700

    add test for TOGGLE_FORM, add switch/case for TOGGLE_FORM to form-visible-reducer, all tests passing

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -1,7 +1,13 @@
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 
 describe('formVisibleReducer', () => {
+
   test('returns default state if no action type is provided', () => {
     expect(formVisibleReducer(false, {type: null})).toEqual(false);
   });
+
+  test('toggles form visibility to true in redux state', () => {
+    expect(formVisibleReducer(false, {type: 'TOGGLE_FORM'})).toEqual(true);
+  })
+
 })
